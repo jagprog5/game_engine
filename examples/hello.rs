627ed63050c4295088440fff2db18cb4f5da889c
John Giorshev <johngiorshev@gmail.com>
2023-12-04 01:40:13 -0500

    .

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,14 +1,17 @@
 extern crate game_engine;
-use std::cell::Cell;
 use rand::prelude::*;
+use std::{cell::Cell, path::PathBuf};
 
-use game_engine::Persistent;
+use game_engine::{GameState, Persistent, Volatile};
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+fn central_rand(radius: f32) -> (f32, f32) {
+    let mut rng = rand::thread_rng();
+    let theta = rng.gen_range(0f32..2f32 * std::f32::consts::PI);
+    (theta.cos() * radius, theta.sin() * radius)
+}
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct CentralSquare {
+struct PrimarySquare {
     // coords origin is center of screen, throughout this example
     x: f32,
     y: f32,
@@ -25,21 +28,19 @@ struct CentralSquare {
     dy_rate: Cell<f32>,
 }
 
-impl CentralSquare {
+impl PrimarySquare {
     fn new() -> Self {
-        let mut rng = rand::thread_rng();
-
-        let x = rng.gen_range(-100.0..=100.0);
-        let y = rng.gen_range(-100.0..=100.0);
-        let dx = rng.gen_range(-1.0..=1.0);
-        let dy = rng.gen_range(-1.0..=1.0);
-
+        let (x, y) = central_rand(200f32);
+        let dist = (x.powi(2) + y.powi(2)).sqrt();
+        let (dx, dy) = (-y / dist, x / dist);
+        // let (dx, dy) = central_rand(1f32);
+        // let 
         let x_rate = Cell::new(0.0);
         let y_rate = Cell::new(0.0);
         let dx_rate = Cell::new(0.0);
         let dy_rate = Cell::new(0.0);
 
-        CentralSquare {
+        PrimarySquare {
             x,
             y,
             dx,
@@ -52,55 +53,142 @@ impl CentralSquare {
     }
 }
 
-impl CentralSquare {
+impl PrimarySquare {
     const SIZE: f32 = 20.;
 }
 
-#[typetag::serde]
-impl Persistent for CentralSquare {
-    fn generate_rate(&self, _persistent: &Vec<Box<dyn Persistent>>) {
+impl Volatile for PrimarySquare {
+    fn generate_rate(&self, _state: &game_engine::GameState) {
         self.x_rate.set(self.dx);
         self.y_rate.set(self.dy);
 
-        let r = (self.x.powi(2) + self.y.powi(2)).sqrt();
-
+        let mut r = (self.x.powi(2) + self.y.powi(2)).sqrt();
+        r = r.max(0.00001);
         self.dx_rate.set(-self.x / r.powi(2));
         self.dy_rate.set(-self.y / r.powi(2));
     }
 
-    fn apply_rate(&mut self) {
+    fn apply_rate(&mut self) -> (bool, Vec<(String, Vec<Box<dyn Volatile>>)>) {
         self.x += self.x_rate.get();
         self.dx += self.dx_rate.get();
         self.y += self.y_rate.get();
         self.dy += self.dy_rate.get();
 
-        // decay:
+        // decay. there's some instability so otherwise it
+        // would fling off into infinite over time
         self.dx *= 0.9995;
         self.dy *= 0.9995;
+
+        let mut to_spawn: Vec<(String, Vec<Box<dyn Volatile>>)> = Vec::new();
+        to_spawn.push((
+            "objects".to_owned(),
+            vec![Box::new(PrimarySquareTail::from(&self))],
+        ));
+        (true, to_spawn)
     }
 
     /// draw to the screen
-    fn render(&self, canvas: &mut sdl2::render::WindowCanvas) {
-        canvas.set_draw_color(sdl2::pixels::Color::GREEN);
-        // let size = canvas.output_size();
-        canvas
-            .draw_rect(sdl2::rect::Rect::new(
-                (self.x - CentralSquare::SIZE / 2.) as i32 + WIDTH as i32 / 2,
-                (self.y - CentralSquare::SIZE / 2.) as i32 + HEIGHT as i32 / 2,
-                CentralSquare::SIZE as u32,
-                CentralSquare::SIZE as u32,
-            ))
-            .unwrap();
+    fn render(&self, _canvas: &mut sdl2::render::WindowCanvas, _window_size: (u32, u32)) {
+        // this entity drawn entirely from particle effects it emitts
+    }
+}
+
+#[typetag::serde]
+impl Persistent for PrimarySquare {}
+
+struct PrimarySquareTail {
+    x: f32,
+    y: f32,
+    dx: f32,
+    dy: f32,
+    x_rate: Cell<f32>,
+    y_rate: Cell<f32>,
+    dx_rate: Cell<f32>,
+    dy_rate: Cell<f32>,
+    alpha: u8,
+}
+
+impl PrimarySquareTail {
+    fn from(from: &PrimarySquare) -> Self {
+        let (drift_x, drift_y) = central_rand(0.2f32);
+        Self {
+            x: from.x,
+            y: from.y,
+            dx: from.dx + drift_x,
+            dy: from.dy + drift_y,
+            x_rate: Cell::new(0f32),
+            y_rate: Cell::new(0f32),
+            dx_rate: Cell::new(0f32),
+            dy_rate: Cell::new(0f32),
+            alpha: 255,
+        }
+    }
+}
+
+impl Volatile for PrimarySquareTail {
+    fn generate_rate(&self, _state: &game_engine::GameState) {
+        self.x_rate.set(self.dx);
+        self.y_rate.set(self.dy);
+        // deviate more and more as the particles expire
+        let progress = (self.alpha) as f32 / 255f32;
+        let (drift_x, drift_y) = central_rand(0.1f32 * (1f32 - progress));
+        self.dx_rate.set(drift_x);
+        self.dy_rate.set(drift_y);
     }
 
-    fn alive(&self) -> bool {
-        return true;
+    fn apply_rate(&mut self) -> (bool, Vec<(String, Vec<Box<dyn Volatile>>)>) {
+        self.x += self.x_rate.get();
+        self.dx += self.dx_rate.get();
+        self.y += self.y_rate.get();
+        self.dy += self.dy_rate.get();
+        self.alpha -= 1;
+        (self.alpha != 0, Vec::new())
+    }
+
+    fn render(&self, canvas: &mut sdl2::render::WindowCanvas, window_size: (u32, u32)) {
+        let progress = (self.alpha) as f32 / 255f32; // from 1 (inclusive) to 0 (exclusive)
+        let size = PrimarySquare::SIZE * progress;
+        let red = (255f32 * (1f32 - progress)) as u8;
+        let green = (255f32 * progress) as u8;
+        let blue = (255f32 * (1f32 - progress)) as u8;
+        let alpha = (255f32 * progress) as u8;
+        canvas.set_draw_color(sdl2::pixels::Color::RGBA(red, green, blue, alpha));
+        canvas
+            .fill_rect(sdl2::rect::Rect::new(
+                (self.x - size / 2.) as i32 + window_size.0 as i32 / 2,
+                (self.y - size / 2.) as i32 + window_size.1 as i32 / 2,
+                size as u32,
+                size as u32,
+            ))
+            .unwrap();
     }
 }
 
 fn main() -> Result<(), String> {
-    let mut state = game_engine::GameState::new("first example", (WIDTH, HEIGHT))?;
-    state.persistent.push(Box::new(CentralSquare::new()));
-    state.run();
+    let mut state = GameState::new(
+        "HELLO WORLD: graphics, persistent / volatile, mouse",
+        (800u32, 600u32),
+        vec!["objects".to_owned()],
+    )?;
+    state
+        .layers
+        .get_mut("objects")
+        .unwrap()
+        .push(Box::new(PrimarySquare::new()));
+    state.run(|event| {
+        match event {
+            sdl2::event::Event::Quit { .. }
+            | sdl2::event::Event::KeyDown {
+                keycode: Some(sdl2::keyboard::Keycode::Escape),
+                ..
+            } => return false,
+            _ => {}
+        }
+        true
+    });
+    let mut save_path: PathBuf = file!().into();
+    save_path.pop();
+    save_path.push("hello_save_file.bin");
+    state.save(save_path.to_str().unwrap().to_owned())?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,11 @@
-use std::default;
-
 extern crate sdl2;
 
 pub struct GameState {
-    /// things which are part of the game loop and saved. items, enemies, etc.
-    pub persistent: Vec<Box<dyn Persistent>>,
+    // order in which layers are rendered
+    pub layer_name_order: Vec<String>,
 
-    /// things which are part of the game loop and NOT saved. particle effects
-    /// drawn UNDERNEATH persistent object
-    pub under_volatile: Vec<Box<dyn Volatile>>,
-
-    /// things which are part of the game loop and NOT saved. particle effects
-    /// drawn OVER persistent object
-    pub over_volatile: Vec<Box<dyn Volatile>>,
+    // all things which are part of the game loop. the name with the things in that layer
+    pub layers: std::collections::BTreeMap<String, Vec<Box<dyn Volatile>>>,
 
     // this is kept track of and always matches the size of canvas. not sure
     // what sort of sys calls happen under the hood for SDL_GetWindowSize. this
@@ -30,24 +23,27 @@ pub struct GameState {
 /// anything which is update in the game loop but not saved. intended for particle effects
 pub trait Volatile {
     /// generate a change in self (rate), which is stored and applied later in this frame.\
-    /// note: this takes a immutable reference to self, maybe store the rate in a Cell.\
+    /// note: this takes an immutable reference to self; maybe store the rate in a Cell.\
     /// note: the rate should not be part of the save file, only the current state.
     fn generate_rate(&self, state: &GameState);
-    
-    /// apply the rate which was previously generated
-    fn apply_rate(&mut self);
+
+    /// apply the rate which was previously generated.\
+    /// first return value is false iff self should be removed from the game\
+    /// second return value is a vector of elements to add to the game\
+    /// and which layers they belong to\
+    fn apply_rate(&mut self) -> (bool, Vec<(String, Vec<Box<dyn Volatile>>)>);
 
     /// draw to the screen
     /// window_size is the size of the canvas
     fn render(&self, canvas: &mut sdl2::render::WindowCanvas, window_size: (u32, u32));
 
-    /// false if should be removed from game loop
-    fn alive(&self) -> bool { true }
+    // allow downcast to Persistent
+    fn as_any(&self) -> &dyn std::any::Any;
 }
 
 /// anything updated in the game loop and saved as part of the save file
 #[typetag::serde(tag = "type")]
-pub trait Persistent: Volatile {}
+pub trait Persistent : Volatile {}
 
 impl GameState {
     /// intended to be used with save() and load().
@@ -71,7 +67,15 @@ impl GameState {
         )
     }
 
-    pub fn new(win_title: &'static str, win_size: (u32, u32)) -> Result<Self, String> {
+    /// create a game state, with associated window and sdl context. \
+    /// `win_title` and `win_size` are used to set the properties of the window \
+    /// `layer_name_order` is the set of layer names to register; used internally to \
+    /// determine rendering order of sprites
+    pub fn new(
+        win_title: &'static str,
+        win_size: (u32, u32),
+        layer_name_order: Vec<String>,
+    ) -> Result<Self, String> {
         let sdl_context = sdl2::init()?;
         let sdl_video_subsystem = sdl_context.video()?;
         let window = sdl_video_subsystem
@@ -80,16 +84,21 @@ impl GameState {
             .position_centered()
             .build()
             .map_err(|e| e.to_string())?;
-        let canvas = window
+        let mut canvas = window
             .into_canvas()
             .present_vsync()
             .build()
             .map_err(|e| e.to_string())?;
+        canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
         let event_pump = sdl_context.event_pump()?;
+
+        let tree: std::collections::BTreeMap<String, Vec<Box<dyn Volatile>>> = layer_name_order
+            .iter()
+            .map(|key| (key.to_owned(), Vec::new()))
+            .collect();
         Ok(Self {
-            persistent: Vec::new(),
-            under_volatile: Vec::new(),
-            over_volatile: Vec::new(),
+            layer_name_order,
+            layers: tree,
             window_width: win_size.0,
             window_height: win_size.1,
             event_pump,
@@ -104,89 +113,119 @@ impl GameState {
         let file = std::fs::File::create(save_file_path).map_err(|e| e.to_string())?;
         let mut writer = std::io::BufWriter::new(file);
 
-        for elem in self.persistent.iter() {
-            serde_json::to_writer(&mut writer, elem).map_err(|e| e.to_string())?;
+        serde_json::to_writer(&mut writer, &self.layer_name_order).map_err(|e| e.to_string())?;
+
+        for name_layer in self.layers.iter() {
+            let s = name_layer.0;
+            serde_json::to_writer(&mut writer, s).map_err(|e| e.to_string())?;
+            for elem in name_layer.1 {
+                if let Ok(persistent_ref) = elem.as_any()
+
+                }
+
+                let as_persistent = match elem.as_any().downcast_ref::<&dyn Persistent>() {
+                    Some(p) => p,
+                    None => continue,
+                };
+                println!("one");
+                serde_json::to_writer(&mut writer, as_persistent).map_err(|e| e.to_string())?;
+            }
         }
         Ok(())
     }
 
     /// reads save file and populates members
     pub fn load(&mut self, path: String) -> Result<(), String> {
-        self.persistent.clear();
-        self.under_volatile.clear();
-        self.over_volatile.clear();
+        self.layer_name_order.clear();
+        self.layers.clear();
 
         let file = std::fs::File::open(path).map_err(|e| e.to_string())?;
         let reader = std::io::BufReader::new(file);
-        self.persistent = serde_json::from_reader(reader)
-            .map_err(|e| format!("Unable to deserialize file: {}", e))?;
+
+        let deserializer = serde_json::Deserializer::from_reader(reader);
+        let iterator = deserializer.into_iter::<serde_json::Value>();
+        for item in iterator {
+            println!("Got {:?}", item.unwrap());
+        }
+        // todo
+
+        // self.layer_name_order = serde_json::from_reader(reader).map_err(|e| e.to_string())?;
+        // while (reader.)
+        // let key: String = serde_json::from_reader(reader).map_err(|e| e.to_string())?;
+
+        // self.persistent = serde_json::from_reader(reader)
+        // .map_err(|e| format!("Unable to deserialize file: {}", e))?;
         Ok(())
     }
 
-    pub fn run(&mut self) {
+    /// f is a closure that handles sdl2 events. returns false iff run should return
+    pub fn run<F>(&mut self, f: F)
+    where
+        F: Fn(sdl2::event::Event) -> bool,
+    {
         let seconds_per_frame = std::time::Duration::from_secs_f32(1f32 / 120f32);
         'outer: loop {
             let start = std::time::Instant::now();
             while let Some(event) = self.event_pump.poll_event() {
-                match event {
-                    sdl2::event::Event::Window {
-                        timestamp: _,
-                        window_id: _,
-                        win_event,
-                    } => match win_event {
-                        sdl2::event::WindowEvent::SizeChanged(x_size, y_size) => {
-                            self.window_width = x_size as u32;
-                            self.window_height = y_size as u32;
-                        }
-                        _ => {}
-                    },
-                    sdl2::event::Event::Quit { .. }
-                    | sdl2::event::Event::KeyDown {
-                        keycode: Some(sdl2::keyboard::Keycode::Escape),
-                        ..
-                    } => break 'outer,
-                    _ => {}
+                // detect for window size change
+                if let sdl2::event::Event::Window {
+                    timestamp: _,
+                    window_id: _,
+                    win_event,
+                } = event
+                {
+                    if let sdl2::event::WindowEvent::SizeChanged(x_size, y_size) = win_event {
+                        self.window_width = x_size as u32;
+                        self.window_height = y_size as u32;
+                    }
+                }
+                // forward all event to the closure
+                if !f(event) {
+                    break 'outer;
                 }
             }
 
-            // order is very important. accumulate rates from states
-            self.persistent.iter().for_each(|c| c.generate_rate(&self));
-            // apply rates to states
-            self.persistent.iter_mut().for_each(|c| c.apply_rate());
+            self.layers
+                .values()
+                .for_each(|layer| layer.iter().for_each(|c| c.generate_rate(&self)));
 
-            // generating and applying rates for particle effects happens after persistent
-            // has acquired its new states
-            self.under_volatile
-                .iter()
-                .chain(self.over_volatile.iter())
-                .for_each(|c| c.generate_rate(&self));
-            // apply rates to states
-            self.under_volatile
-                .iter_mut()
-                .chain(self.over_volatile.iter_mut())
-                .for_each(|c| c.apply_rate());
+            let mut spawned: Vec<(String, Vec<Box<dyn Volatile>>)> = Vec::new();
 
-            // check removing particle effects first.
-            // cleanup order important as a volatile element may check if a persistent is dead then remove itself
-            self.under_volatile.retain(|e| e.alive());
-            self.over_volatile.retain(|e| e.alive());
-            // remove persistent after removing volatile.
-            self.persistent.retain(|e| e.alive());
-
-            // render all after update
+            for layer_name in self.layer_name_order.iter() {
+                match self.layers.get_mut(layer_name) {
+                    Some(layer) => {
+                        let len = layer.len();
+                        for i in (0..len).rev() {
+                            let elem = &mut layer[i];
+                            let mut val = elem.apply_rate();
+                            if !val.0 {
+                                layer.remove(i);
+                            }
+                            spawned.append(&mut val.1);
+                        }
+                    }
+                    None => {}
+                }
+            }
             self.canvas.set_draw_color(sdl2::pixels::Color::BLACK);
             self.canvas.clear();
 
-            self.under_volatile
-                .iter_mut()
-                .for_each(|c| c.render(&mut self.canvas, (self.window_width, self.window_height)));
-            self.persistent
-                .iter_mut()
-                .for_each(|c| c.render(&mut self.canvas, (self.window_width, self.window_height)));
-            self.over_volatile
-                .iter_mut()
-                .for_each(|c| c.render(&mut self.canvas, (self.window_width, self.window_height)));
+            self.layers.values().for_each(|layer| {
+                layer.iter().for_each(|c| {
+                    c.render(&mut self.canvas, (self.window_width, self.window_height))
+                })
+            });
+
             self.canvas.present();
+
+            // insert the spawned elements after the new states are available
+            for mut s in spawned {
+                let layer = self
+                    .layers
+                    .get_mut(&s.0)
+                    .expect(&format!("Tried spawning to unregisted layer: {}", &s.0));
+                layer.append(&mut s.1);
+            }
 
             let stop = std::time::Instant::now();
             let duration = stop - start;
